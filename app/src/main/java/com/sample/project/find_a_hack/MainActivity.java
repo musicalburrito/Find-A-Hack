de86597400e67b1c543a55c84782b61abbbc4f39
nli006 <nli006@ucr.edu>
2017-04-09 00:23:52 -0700

    firebase database example

--- app/src/main/java/com/sample/project/find_a_hack/MainActivity.java
+++ app/src/main/java/com/sample/project/find_a_hack/MainActivity.java
@@ -1,7 +1,25 @@
 package com.sample.project.find_a_hack;
 
+<<<<<<< Updated upstream
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+=======
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+
+import java.lang.String;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+>>>>>>> Stashed changes
 
 public class MainActivity extends AppCompatActivity {
 
