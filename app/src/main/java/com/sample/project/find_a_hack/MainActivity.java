7e44f3721d76e45c473b9b701bf6bf17de931e58
nli006 <nli006@ucr.edu>
2017-04-09 00:27:33 -0700

    firebase database example

--- app/src/main/java/com/sample/project/find_a_hack/MainActivity.java
+++ app/src/main/java/com/sample/project/find_a_hack/MainActivity.java
@@ -1,9 +1,5 @@
 package com.sample.project.find_a_hack;
 
-<<<<<<< Updated upstream
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-=======
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,13 +15,46 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
->>>>>>> Stashed changes
 
 public class MainActivity extends AppCompatActivity {
+
+    /* get instance of database. needs to be a final attribute of class or a global variable to be used
+       in the functions.
+     */
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* sets button variable */
+        Button button;
+        button = (Button) findViewById(R.id.usernamebutton2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /* sets text data variable and stores it into database upon button click.
+                 changed type to string to make it easier to debug can store any class. */
+                EditText textData;
+                textData = (EditText) findViewById(R.id.editText);
+                myRef.setValue(textData.getText().toString());
+            }
+        });
+
+        /* listens for data change. outputs data from database.  */
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("hello", "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d("fail", "failed to read value", databaseError.toException());
+            }
+        });
     }
 }
